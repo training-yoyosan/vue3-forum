fbc2a7ce62259a74aa62bfead7051c61ea575690
Marius Palade <contact@mpalade.ro>
2021-11-15 22:50:14 +0200

    add dummy data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -99,14 +99,24 @@
         >
       </li>
     </ul>
+
+    {{ sourceData }}
   </div>
 </template>
 
 <script>
+import sourceData from "@/data.json";
+
+console.log(sourceData);
 export default {
   name: "HelloWorld",
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      sourceData,
+    };
   },
 };
 </script>
@@ -116,14 +126,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
---
+++ src/components/PageHome.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script>
+export default {
+  name: "PageHome",
+};
+</script>
+
+<style scoped></style>
