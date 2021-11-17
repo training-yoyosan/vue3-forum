3312b2f0f6b94fd3d565ed2e27eca4973faf93ae
Marius Palade <contact@mpalade.ro>
2021-11-17 21:54:53 +0200

    display thread posts and user

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -3,6 +3,12 @@
 
   <div v-for="thread in threads" :key="thread.id">
     <h2>{{ thread.title }}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>
+        {{ userById(postById(postId).userId).name }}
+      </p>
+      <p>{{ postById(postId).text }}</p>
+    </div>
   </div>
 </template>
 
@@ -18,6 +24,14 @@ export default {
       posts: sourceData.posts,
       users: sourceData.users,
     };
+  },
+  methods: {
+    postById(postId) {
+      return this.posts.find((p) => p.id === postId);
+    },
+    userById(userId) {
+      return this.users.find((p) => p.id === userId);
+    },
   },
 };
 </script>
