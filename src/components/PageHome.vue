bf6d6789d3839eb720bb1971f2935e9663d19eea
Marius Palade <contact@mpalade.ro>
2021-11-23 21:45:05 +0200

    move thread list into a component and add mockup

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,59 +1,21 @@
 <template>
-  <div class="col-large push-top" v-for="thread in threads" :key="thread.id">
-    <h1>{{ thread.title }}</h1>
-
-    <div class="post-list">
-      <div class="post" v-for="postId in thread.posts" :key="postId">
-        <div class="user-info">
-          <a href="#" class="user-name">
-            {{ userById(postById(postId).userId).name }}
-          </a>
-
-          <a href="#">
-            <img
-              class="avatar-large"
-              :src="userById(postById(postId).userId).avatar"
-              alt=""
-            />
-          </a>
-
-          <p class="desktop-only text-small">107 posts</p>
-        </div>
-
-        <div class="post-content">
-          <div>
-            <p>{{ postById(postId).text }}</p>
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          {{ postById(postId).publishedAt }}
-        </div>
-      </div>
-    </div>
-  </div>
+  <h1>Welcome to the Forum!</h1>
+  <ThreadList :threads="threads" />
 </template>
 
 <script>
 import sourceData from "@/data.json";
+import ThreadList from "@/components/ThreadList";
 
 export default {
   name: "PageHome",
 
+  components: { ThreadList },
+
   data() {
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts,
-      users: sourceData.users,
     };
-  },
-  methods: {
-    postById(postId) {
-      return this.posts.find((p) => p.id === postId);
-    },
-    userById(userId) {
-      return this.users.find((p) => p.id === userId);
-    },
   },
 };
 </script>
