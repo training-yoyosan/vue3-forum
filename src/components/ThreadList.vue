865ab65def86a892c7b2b4793b89ededa87dce11
Marius Palade <contact@mpalade.ro>
2021-11-24 19:34:25 +0200

    link to show a thread

--- src/components/ThreadList.vue
+++ src/components/ThreadList.vue
@@ -6,7 +6,9 @@
       <div class="thread" v-for="thread in threads" :key="thread.id">
         <div>
           <p>
-            <a href="#">{{ thread.title }}</a>
+            <router-link :to="{ name: 'ThreadShow', params: { id: thread.id } }"
+              >{{ thread.title }}
+            </router-link>
           </p>
           <p class="text-faded text-xsmall">
             By <a href="#">{{ userById(thread.userId).name }}</a
