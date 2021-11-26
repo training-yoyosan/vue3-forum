e80b25ee146f486d5a05da5b3e704c784fa80dd8
Marius Palade <contact@mpalade.ro>
2021-11-26 18:34:40 +0200

    add PostEditor component

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="col-full push-top">
+    <form @submit.prevent="addPost">
+      <div class="form-group">
+        <label for="thread_content">Content:</label>
+        <textarea
+          id="thread_content"
+          class="form-input"
+          name="content"
+          rows="8"
+          cols="140"
+          v-model="text"
+        ></textarea>
+      </div>
+
+      <div class="btn-group">
+        <button class="btn btn-blue" type="submit" name="submit">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { v4 } from "uuid";
+
+export default {
+  name: "PostEditor",
+
+  data() {
+    return {
+      text: "",
+    };
+  },
+
+  methods: {
+    addPost() {
+      const id = v4();
+
+      const post = {
+        id,
+        text: this.text,
+        publishedAt: Math.floor(Date.now() / 1000),
+        userId: "Miej9zSGMRZKDvMXzfxjVOyv3RF3",
+      };
+
+      this.$emit("save", { post });
+
+      this.text = "";
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/ThreadShow.vue
+++ src/views/ThreadShow.vue
@@ -4,41 +4,21 @@
 
     <post-list :posts="threadPosts" />
 
-    <div class="col-full push-top">
-      <form @submit.prevent="addPost">
-        <div class="form-group">
-          <label for="thread_content">Content:</label>
-          <textarea
-            id="thread_content"
-            class="form-input"
-            name="content"
-            rows="8"
-            cols="140"
-            v-model="newPostText"
-          ></textarea>
-        </div>
-
-        <div class="btn-group">
-          <button class="btn btn-ghost">Cancel</button>
-          <button class="btn btn-blue" type="submit" name="Publish">
-            Publish
-          </button>
-        </div>
-      </form>
-    </div>
+    <post-editor @save="addPost" />
   </div>
 </template>
 
 <script>
 import sourceData from "@/data.json";
 import PostList from "@/components/PostList";
-import { v4 } from "uuid";
+import PostEditor from "@/components/PostEditor";
 
 export default {
   name: "ThreadShow",
 
   components: {
     PostList,
+    PostEditor,
   },
 
   props: {
@@ -52,7 +32,6 @@ export default {
     return {
       threads: sourceData.threads,
       posts: sourceData.posts,
-      newPostText: "",
     };
   },
 
@@ -66,21 +45,14 @@ export default {
   },
 
   methods: {
-    addPost() {
-      const id = v4();
-
+    addPost(eventData) {
       const post = {
-        id,
-        text: this.newPostText,
-        publishedAt: Math.floor(Date.now() / 1000),
+        ...eventData.post,
         threadId: this.id,
-        userId: "Miej9zSGMRZKDvMXzfxjVOyv3RF3",
       };
 
       this.posts.push(post);
-      this.thread.posts.push(id);
-
-      this.newPostText = "";
+      this.thread.posts.push(post.id);
     },
   },
 };
