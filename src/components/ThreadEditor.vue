076ea6fd5295d81a58eb15e90cf7166600f83703
Marius Palade <contact@mpalade.ro>
2021-12-06 15:54:48 +0200

    move thread editing into a component

---
+++ src/components/ThreadEditor.vue
@@ -0,0 +1,54 @@
+<template>
+  <form @submit.prevent="save">
+    <div class="form-group">
+      <label for="thread_title">Title:</label>
+      <input
+        v-model="title"
+        type="text"
+        id="thread_title"
+        class="form-input"
+        name="title"
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="thread_content">Content:</label>
+      <textarea
+        v-model="text"
+        id="thread_content"
+        class="form-input"
+        name="content"
+        rows="8"
+        cols="140"
+      ></textarea>
+    </div>
+
+    <div class="btn-group">
+      <button class="btn btn-ghost" type="reset" @click="$emit('cancel')">
+        Cancel
+      </button>
+      <button class="btn btn-blue" type="submit" name="Publish">Publish</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "ThreadEditor",
+
+  data() {
+    return {
+      title: "",
+      text: "",
+    };
+  },
+
+  methods: {
+    save() {
+      this.$emit("save", { text: this.text, title: this.title });
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/ThreadCreate.vue
+++ src/views/ThreadCreate.vue
@@ -4,46 +4,16 @@
       Create new thread in <i>{{ forum.name }}</i>
     </h1>
 
-    <form @submit.prevent="save">
-      <div class="form-group">
-        <label for="thread_title">Title:</label>
-        <input
-          v-model="title"
-          type="text"
-          id="thread_title"
-          class="form-input"
-          name="title"
-        />
-      </div>
-
-      <div class="form-group">
-        <label for="thread_content">Content:</label>
-        <textarea
-          v-model="text"
-          id="thread_content"
-          class="form-input"
-          name="content"
-          rows="8"
-          cols="140"
-        ></textarea>
-      </div>
-
-      <div class="btn-group">
-        <button class="btn btn-ghost" type="reset" @click="cancel">
-          Cancel
-        </button>
-        <button class="btn btn-blue" type="submit" name="Publish">
-          Publish
-        </button>
-      </div>
-    </form>
+    <ThreadEditor @save="save" @cancel="cancel" />
   </div>
 </template>
 
 <script>
+import ThreadEditor from "@/components/ThreadEditor";
+
 export default {
   name: "ThreadCreate",
-
+  components: { ThreadEditor },
   props: {
     forumId: {
       type: String,
@@ -59,19 +29,12 @@ export default {
     },
   },
 
-  data() {
-    return {
-      title: "",
-      text: "",
-    };
-  },
-
   methods: {
-    async save() {
+    async save({ title, text }) {
       const thread = await this.$store.dispatch("createThread", {
         forumId: this.forum.id,
-        title: this.title,
-        text: this.text,
+        title,
+        text,
       });
 
       this.$router.push({ name: "ThreadShow", params: { id: thread.id } });
