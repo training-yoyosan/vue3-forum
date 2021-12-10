8e99828acd32b566474219a19bc1e925b9b91b0f
Marius Palade <contact@mpalade.ro>
2021-12-10 20:45:15 +0200

    setup firestore and import data

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: "AIzaSyC7-F0txZFzZ4QKD9zm7ACaV7rnM1JdJTo",
+  authDomain: "vue3-forum-b0fb9.firebaseapp.com",
+  projectId: "vue3-forum-b0fb9",
+  storageBucket: "vue3-forum-b0fb9.appspot.com",
+  messagingSenderId: "239987237496",
+  appId: "1:239987237496:web:bed380de907f07af4ec38d",
+};
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "@/config/firebase";
+
+initializeApp(firebaseConfig);
 
 const app = createApp(App);
 app.use(store);
