63ea5b9553e3b636350e090d9f1f52d3e80919d1
Yoyo San <veselu@gmail.com>
2021-12-28 09:41:30 +0200

    adjust user dropdown menu

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -15,27 +15,27 @@
     <nav class="navbar">
       <ul>
         <li v-if="authUser" class="navbar-user">
-          <router-link :to="{ name: 'Profile' }">
+          <a @click.prevent="userDropdownOpen = !userDropdownOpen">
             <img class="avatar-small" :src="authUser.avatar" :alt="`${authUser.name} profile picture`" />
             <span>
               {{ authUser.name }}
               <img class="icon-profile" src="../assets/svg/arrow-profile.svg" alt="" />
             </span>
-          </router-link>
+          </a>
 
           <!-- dropdown menu -->
           <!-- add class "active-drop" to show the dropdown -->
-          <div id="user-dropdown">
+          <div id="user-dropdown" :class="{ 'active-drop': userDropdownOpen }">
             <div class="triangle-drop"></div>
             <ul class="dropdown-menu">
               <li class="dropdown-menu-item">
-                <a href="profile.html">View profile</a>
+                <router-link :to="{ name: 'Profile' }">View profile</router-link>
               </li>
-              <li class="dropdown-menu-item"><a href="#">Log out</a></li>
+              <li class="dropdown-menu-item"><a @click.prevent="$store.dispatch('signOut')">Sign Out</a></li>
             </ul>
           </div>
         </li>
-        <li v-if="authUser" class="navbar-item"><a @click.prevent="$store.dispatch('signOut')">Sign Out</a></li>
+
         <li v-if="!authUser" class="navbar-item"><router-link :to="{ name: 'SignIn' }">Sign In</router-link></li>
         <li v-if="!authUser" class="navbar-item"><router-link :to="{ name: 'Register' }">Register</router-link></li>
       </ul>
@@ -70,6 +70,12 @@ import { mapGetters } from "vuex";
 
 export default {
   name: "TheNavbar",
+
+  data() {
+    return {
+      userDropdownOpen: false,
+    };
+  },
 
   computed: {
     ...mapGetters(["authUser"]),
